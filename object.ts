67f67b172fa79c3ce815f6c6f74f4c798595267a
our ame <djdjdjdj@f.kcic>
2024-10-10 17:20:09 +0300

    add: obj

---
+++ object.ts
@@ -0,0 +1,14 @@
+const characteristicsOfTheMachine = {
+salon: 'темный' ,
+assistanceSystems: 'датчик дождя',
+interior: 'панорамная крыша',
+headlights: 'светодиодные',
+comfort:  'Автозапуск двигателя',
+securitySystems: 'Сигнализация',
+}
+
+characteristicsOfTheMachine.salon = 'светлый'
+
+console.log(characteristicsOfTheMachine.salon)
+
+console.log(characteristicsOfTheMachine)
